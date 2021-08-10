d74365cdbdc3c63a93fe55af826adbf28f425098
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-08-10 17:56:00 -0500

    :art: [ADD] Created a list of tasks by user

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,13 +1,45 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { getAllTasks } from '../../actions/tasksActions'
+import { Error, Loader } from '../'
+// *Styles
+import { TasksContainer } from '../../styles/components/Tasks'
 
-const Tasks = (props) => {
+const Tasks = ({ tasks, loading, error, getAllTasks }) => {
   useEffect(() => {
-    props.getAllTasks()
+    if (!Object.keys(tasks).length) getAllTasks() // !If we don't have tasks, go for them
   }, [])
-  console.log(props)
-  return <div>Tasks</div>
+
+  // ?We cant simplify the code creating two more components. One for Task container and other for task
+  const addYTasksByUser = (userId) => {
+    const tasksByUser = {
+      ...tasks[userId],
+    }
+    return Object.keys(tasksByUser).map((taskId) => (
+      <div>
+        <input type='checkbox' defaultChecked={tasksByUser[taskId].completed} />
+        {tasksByUser[taskId].title}
+      </div>
+    ))
+  }
+
+  return (
+    <>
+      {error && <Error text={error.message} />}
+      {loading && <Loader />}
+      {!error && !loading && tasks && (
+        <div>
+          {/* If we have an Object, the parameters in the map function are the variables inside the data */}
+          {Object.keys(tasks).map((userId) => (
+            <div key={userId}>
+              <h3>User {userId}:</h3>
+              <TasksContainer>{addYTasksByUser(userId)}</TasksContainer>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
 const mapStateToProps = ({ tasksReducers }) => tasksReducers
---
+++ src/styles/components/Tasks.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const TasksContainer = styled.div`
+  font-size: 14px;
+  margin-left: 50px;
+  margin-bottom: 30px;
+`
